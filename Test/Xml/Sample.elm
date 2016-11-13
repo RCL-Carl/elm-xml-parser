8e3802ceb33a443209b2a5f4ba693037470c72ee
Carl Allendorph <carl@resonantcircuitlabs.com>
2016-11-13 09:38:41 -0800

    Added a sample XSD schema string for testing and example purposes

--- Test/Xml/Sample.elm
+++ Test/Xml/Sample.elm
@@ -139,3 +139,31 @@ sample2 =
   </message>
 </error>
   """
+
+xsdSample : String
+xsdSample =
+  """
+<?xml version="1.0"?>
+<xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema">
+
+  <xs:annotation>
+    <xs:appinfo> Blarg
+    </xs:appinfo>
+    <xs:documentation>
+    Schema Content Docs
+    </xs:documentation>
+  </xs:annotation>
+
+  <xs:element name="note">
+    <xs:complexType>
+      <xs:sequence>
+        <xs:element name="to" type="xs:string"/>
+        <xs:element name="from" type="xs:string"/>
+        <xs:element name="heading" type="xs:string"/>
+        <xs:element name="body" type="xs:string"/>
+      </xs:sequence>
+    </xs:complexType>
+  </xs:element>
+
+</xs:schema>
+  """
